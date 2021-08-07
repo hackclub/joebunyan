b50addda05941ade6050537a7e8fec52286bfbd9
Rishi Kothari <itsrishikothari@gmail.com>
2021-08-07 14:05:31 -0400

    feat(sounds): :sparkles: Add initial files

---
+++ sounds/pages/_app.js
@@ -0,0 +1,3 @@
+const App = ({ Component, pageProps }) => <Component {...pageProps} />
+
+export default App
---
+++ sounds/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			Hi there!
+		</div>
+	)
+}
