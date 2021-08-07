8f490cbd33e4292df996f8ee78e3e13a60ab8a38
Caleb Denio <cjdenio44@gmail.com>
2021-08-07 14:44:31 -0400

    Make le API

---
+++ api/go.mod
@@ -0,0 +1,9 @@
+module github.com/hackclub/joebunyan/api
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.3 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/slack-go/slack v0.9.4 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/slack-go/slack/slackevents"
+)
+
+var connections map[*websocket.Conn]bool = make(map[*websocket.Conn]bool)
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/slack/events", func(c *gin.Context) {
+		body, err := io.ReadAll(c.Request.Body)
+		if err != nil {
+			log.Println(err)
+			c.String(500, "sadge")
+			return
+		}
+
+		event, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+		if err != nil {
+			log.Println(err)
+			c.String(500, "sadge")
+			return
+		}
+
+		if event.Type == slackevents.URLVerification {
+			var r *slackevents.ChallengeResponse
+
+			err := json.Unmarshal(body, &r)
+			if err != nil {
+				log.Println(err)
+				c.String(500, "sadge")
+				return
+			}
+
+			c.String(200, r.Challenge)
+		} else if event.Type == slackevents.CallbackEvent {
+			innerEvent := event.InnerEvent
+			switch ev := innerEvent.Data.(type) {
+			case *slackevents.MessageEvent:
+				if ev.SubType == "" {
+					fmt.Println(ev.Text)
+
+					for v := range connections {
+						v.WriteJSON(map[string]interface{}{
+							"type":    "message",
+							"text":    ev.Text,
+							"channel": ev.Channel,
+							"user":    ev.User,
+						})
+					}
+				}
+			}
+		}
+	})
+
+	r.GET("/stream", func(c *gin.Context) {
+		upgrader := websocket.Upgrader{}
+
+		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			log.Println(err)
+			c.String(500, "sadge")
+			return
+		}
+
+		connections[ws] = true
+
+		for {
+			_, _, err := ws.ReadMessage()
+			if err != nil {
+				delete(connections, ws)
+				log.Println("Client disconnected")
+				break
+			}
+		}
+	})
+
+	r.Run(":3000")
+}
