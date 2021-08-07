edf633620afc51d987d0414f8e2435b857a49d43
Matt Gleich <git@mattglei.ch>
2021-08-07 16:14:53 -0400

    feat: main websocket reading and led updates
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- see/main.go
+++ see/main.go
@@ -1,7 +1,67 @@
 package main
 
-import "github.com/gleich/lumber"
+import (
+	"encoding/json"
+	"math/rand"
+	"net/url"
+	"os"
+	"os/signal"
+
+	"github.com/gleich/lumber"
+	"github.com/gorilla/websocket"
+	"github.com/nathany/bobblehat/sense/screen"
+	"github.com/nathany/bobblehat/sense/screen/color"
+)
+
+type event struct {
+	channel string
+	text    string
+	Type    string
+	user    string
+}
 
 func main() {
 	lumber.Info("Booted up!")
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	u := url.URL{Scheme: "ws", Host: "joebunyan.ngrok.io", Path: "stream"}
+	lumber.Info("Connecting to", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		lumber.Fatal(err, "Failed to connect to", u.String())
+	}
+
+	fb := screen.NewFrameBuffer()
+	err = screen.Clear()
+	if err != nil {
+		lumber.Fatal(err, "Failed to clear screen")
+	}
+
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			lumber.Error(err, "Failed to read message")
+		}
+
+		lumber.Success("Got message")
+
+		var data event
+		err = json.Unmarshal(message, &data)
+		if err != nil {
+			lumber.Error(err, "Failed to unmarshal", message)
+		}
+
+		if data.Type == "message" {
+			fb.SetPixel(rand.Intn(8), rand.Intn(8), color.White)
+		} else {
+			fb.SetPixel(rand.Intn(8), rand.Intn(8), color.Green)
+		}
+
+		err = screen.Draw(fb)
+		if err != nil {
+			lumber.Error(err, "Failed to update screen")
+		}
+	}
 }
