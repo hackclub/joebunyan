2da6f502d7c5847e052b29bc234bea670da9dab3
Rishi Kothari <itsrishikothari@gmail.com>
2021-08-07 14:06:06 -0400

    merge changes

---
+++ lights/target/debug/deps/lights-110787cd6eb42f7a.d
@@ -0,0 +1,5 @@
+/Users/rishikothari/Documents/Code/hackclub/2021/joebunyan/lights/target/debug/deps/lights-110787cd6eb42f7a.rmeta: src/main.rs
+
+/Users/rishikothari/Documents/Code/hackclub/2021/joebunyan/lights/target/debug/deps/lights-110787cd6eb42f7a.d: src/main.rs
+
+src/main.rs:
---
+++ lights/target/debug/deps/lights-b24117469445df97.d
@@ -0,0 +1,5 @@
+/Users/rishikothari/Documents/Code/hackclub/2021/joebunyan/lights/target/debug/deps/lights-b24117469445df97.rmeta: src/main.rs
+
+/Users/rishikothari/Documents/Code/hackclub/2021/joebunyan/lights/target/debug/deps/lights-b24117469445df97.d: src/main.rs
+
+src/main.rs:
