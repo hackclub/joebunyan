49164b4dbe031e133b6b892718816723c4c8ad4d
Matt Gleich <git@mattglei.ch>
2021-08-08 22:54:24 -0400

    feat: clean up coordinate
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- see/main.go
+++ see/main.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/gleich/lumber"
 	"github.com/gorilla/websocket"
@@ -20,12 +21,17 @@ type event struct {
 	user    string
 }
 
+type pixel struct {
+	channels []string
+	updated  time.Time
+}
+
 func main() {
 	lumber.Info("Booted up!")
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
-	u := url.URL{Scheme: "ws", Host: "joebunyan.ngrok.io", Path: "stream"}
+	u := url.URL{Scheme: "wss", Host: "joebunyan.haas.hackclub.com", Path: "stream"}
 	lumber.Info("Connecting to", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
@@ -40,28 +46,45 @@ func main() {
 	}
 
 	for {
+		// Getting message
 		_, message, err := c.ReadMessage()
 		if err != nil {
 			lumber.Error(err, "Failed to read message")
 		}
+		lumber.Success("Got message", string(message))
 
-		lumber.Success("Got message")
-
+		// Parsing JSON from event
 		var data event
 		err = json.Unmarshal(message, &data)
 		if err != nil {
 			lumber.Error(err, "Failed to unmarshal", message)
 		}
 
+		// Setting pixel
+		x := rand.Intn(8)
+		y := rand.Intn(8)
 		if data.Type == "message" {
-			fb.SetPixel(rand.Intn(8), rand.Intn(8), color.White)
+			fb.SetPixel(x, y, color.White)
+		} else if data.Type == "ping" {
+			continue
 		} else {
-			fb.SetPixel(rand.Intn(8), rand.Intn(8), color.Green)
+			fb.SetPixel(x, y, color.Green)
 		}
 
+		// Updating screen
 		err = screen.Draw(fb)
 		if err != nil {
 			lumber.Error(err, "Failed to update screen")
 		}
+
+		// Having the pixel not update for 100 milliseconds
+		go func() {
+			time.Sleep(300 * time.Millisecond)
+			fb.SetPixel(x, y, color.Black)
+			err = screen.Draw(fb)
+			if err != nil {
+				lumber.Error(err, "Failed to set", x, ",", y, "coordinates")
+			}
+		}()
 	}
 }
